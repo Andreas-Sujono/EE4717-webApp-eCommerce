d62666ec03f5a0574900c56a8b20410cfe6fe97b
Andreas_Sujono <andreassujono@gmail.com>
2020-09-30 01:15:27 +0800

    finished shopping cart page

---
+++ components/nav.php
@@ -0,0 +1,27 @@
+<nav>
+    <div class="nav-content">
+        <div class="logo">
+            <img src="../images/logo-white.png"/>
+        </div>
+        <ul>
+            <li>Laptop<li>
+            <li>Phone<li>
+            <li>Tablet<li>
+            <li>Accessories<li>
+        </ul>
+        <div class="right-content">
+            <div class="wishlist">
+                <img src="../images/wishlist.png" alt="wishlist"/>
+                <div>Wishlist</div>
+            </div>
+            <div class="cart">
+                <img src="../images/cart.png" alt="cart"/>
+                <div>Cart</div>
+            </div>
+            <div class="account">
+                <img src="../images/account.png" alt="account"/>
+                <div>Account</div>
+            </div>
+        </div>
+    </div>
+</nav>
--- pages/shoppingCart.php
+++ pages/shoppingCart.php
@@ -2,11 +2,76 @@
     <head>
         <title>Shopping cart</title>
         <link rel="stylesheet" href="../css/index.css"/>
+        <link rel="stylesheet" href="../css/nav.css"/>
         <link rel="stylesheet" href="../css/footer.css"/>
+        <link rel="stylesheet" href="../css/shoppingCart.css"/>
     </head>
 
     <body>
-        <h1>Shopping cart</h1>
+        <?php include '../components/nav.php' ?>
+            <div class="shopping-cart-page">
+                <div class="title-container">
+                    <h1>Shopping cart</h1>
+                </div>
+                <div class="table-container">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Product</th>
+                                <th>Quantity</th>
+                                <th>Unit Price</th>
+                                <th>Subtotal Price</th>
+                                <th>Action</th>
+                            </tr>   
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="product-col"> 
+                                    <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3oYjc?ver=e1aa" alt="product"/>
+                                    <span>Asus Vivobook<span>
+                                </td>
+                                <td>1</td>
+                                <td>$250</td>
+                                <td>$250</td>
+                                <td>
+                                    <button class="delete-btn"> Delete </button>
+                                </td>
+                            </tr>   
+                            <tr>
+                                <td class="product-col"> 
+                                    <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3oYjc?ver=e1aa" alt="product"/>
+                                    <span>Asus Vivobook<span>
+                                </td>
+                                <td>1</td>
+                                <td>$250</td>
+                                <td>$250</td>
+                                <td>
+                                    <button class="delete-btn"> Delete </button>
+                                </td>
+                            </tr>   
+                            <tr>
+                                <td class="product-col"> 
+                                    <img src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3oYjc?ver=e1aa" alt="product"/>
+                                    <span>Asus Vivobook<span>
+                                </td>
+                                <td>1</td>
+                                <td>$250</td>
+                                <td>$250</td>
+                                <td>
+                                    <button class="delete-btn"> Delete </button>
+                                </td>
+                            </tr>   
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="4" class="subtotal-price">Subtotal Price: <span>$250</span></td>
+                                <td><button class="checkout-btn"> Checkout </button></td>
+                            </tr>   
+                        </tfoot>
+                    </table>
+                </div>
+
+            </div>
         <?php include '../components/footer.php' ?>
     </body>
 
