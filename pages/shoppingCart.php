e3e869fbe2649c2c3e1cb4698dafaf9a5201a6c8
Andreas_Sujono <andreassujono@gmail.com>
2020-09-29 23:50:16 +0800

    added footer component

--- pages/shoppingCart.php
+++ pages/shoppingCart.php
@@ -1,13 +1,13 @@
 <html>
     <head>
         <title>Shopping cart</title>
+        <link rel="stylesheet" href="../css/index.css"/>
         <link rel="stylesheet" href="../css/footer.css"/>
-
     </head>
 
     <body>
         <h1>Shopping cart</h1>
-        <?php include '../components/footer.php';?>
+        <?php include '../components/footer.php' ?>
     </body>
 
 </html>
